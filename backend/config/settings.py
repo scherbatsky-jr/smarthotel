cb4d5954ed0dfebf0feaf3540a7a787e680c0a14
Sunil Prajapati <sunilpzpt@gmail.com>
2025-04-06 19:02:48 +0700

    fix: cors for docker app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -134,6 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    os.getenv("APP_URL")
 ]
 
 MEDIA_URL = '/media/'
